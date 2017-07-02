ff2d4e5374bae91c8c5baca6c4723b4c50b3d304
PhantomMatthew <jiabei.yuan@gmail.com>
2017-07-02 19:12:38 +0800

    Update Python Drive

--- python3/PyDrive.py
+++ python3/PyDrive.py
@@ -1,8 +1,13 @@
 from ev3dev.auto import *
 #import time
 
-m1 = Motor(OUTPUT_B)
-m2 = Motor(OUTPUT_C)
+#m1 = Motor(OUTPUT_B)
+#m2 = Motor(OUTPUT_C)
 
-m1.run_timed(time_sp=100, speed_sp=500)
-m2.run_timed(time_sp=100, speed_sp=500)
+#m1.run_timed(time_sp=100, speed_sp=500)
+#m2.run_timed(time_sp=100, speed_sp=500)
+
+motors = [LargeMotor(address) for address in (OUTPUT_B, OUTPUT_C)]
+
+for m in motors:
+    m.run_timed(time_sp = 1000, speed_sp = -600)
